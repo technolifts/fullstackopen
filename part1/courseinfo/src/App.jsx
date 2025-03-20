3bf61f746c20d9a29f44f8b796d53a298d429d7e
technolifts <ryan.acarroll@yahoo.com>
2025-03-19 21:29:47 -0700

    Complete 1.7 and 1.8

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,12 +6,33 @@ const Button = (props) => (
   </button>
 )
 
+const average = (props) => props.all === 0 ? 0 : (props.good - props.bad) / props.all
+const positivePercentage = (good, all) => all === 0 ? 0 : (good / all) * 100
+
+const Statistics = ({good, neutral, bad, all}) => {
+  return (
+    <div>    
+      <h1>statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average({good, bad, all})}</p>
+      <p>positive: {positivePercentage(good, all)} %</p>
+    </div>
+    )
+  }
+
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+
+
+  
 
   return (
     <div>
@@ -19,10 +40,7 @@ const App = () => {
       <Button onClick={() => setGood(good +1 )} text="Good"/>
       <Button onClick={() => setNeutral(neutral + 1)} text="Neutral"/>
       <Button onClick={() => setBad(bad + 1)} text="Bad"/>
-      <h1>statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
     </div>
   )
 }

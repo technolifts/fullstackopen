d876547dcfe0bcc06b1fd5b35a9615e36fe57e2f
technolifts <ryan.acarroll@yahoo.com>
2025-03-19 21:38:23 -0700

    Complete 1.9

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,6 +10,14 @@ const average = (props) => props.all === 0 ? 0 : (props.good - props.bad) / prop
 const positivePercentage = (good, all) => all === 0 ? 0 : (good / all) * 100
 
 const Statistics = ({good, neutral, bad, all}) => {
+  if( all === 0 )
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+
   return (
     <div>    
       <h1>statistics</h1>

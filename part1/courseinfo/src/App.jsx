27d4a16637196b51f9406bffa630cc1d3e53bce1
technolifts <ryan.acarroll@yahoo.com>
2025-03-19 21:05:15 -0700

    Complete 1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +15,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good +1 )} text="Good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral"/>
+      <Button onClick={() => setBad(bad + 1)} text="Bad"/>
+      <h1>statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }

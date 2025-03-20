6c0bbe528d8952ef623021cc3428582d7d66d7dc
technolifts <ryan.acarroll@yahoo.com>
2025-03-19 22:01:09 -0700

    Complete 1.11

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,15 +6,16 @@ const Button = (props) => (
   </button>
 )
 
-const average = (props) => props.all === 0 ? 0 : (props.good - props.bad) / props.all
+const average = (good, bad, all) => all === 0 ? 0 : (good - bad) / all
 const positivePercentage = (good, all) => all === 0 ? 0 : (good / all) * 100
 
-const StatisticsLine = (props) => {
-  if (props.text === "positive") {
-    return <p>{props.text}: {props.value} %</p>
-  }
-  return <p>{props.text}: {props.value}</p>
-}
+const StatisticsLine = (props) => (
+  
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.value}{props.percentage ? ' %' : ''}</td>
+  </tr>
+)
 
 const Statistics = ({good, neutral, bad, all}) => {
   if( all === 0 )
@@ -28,18 +29,16 @@ const Statistics = ({good, neutral, bad, all}) => {
   return (
     <div>    
       <h1>statistics</h1>
-      <p>Good: {good}</p>
-      <StatisticsLine text="good" value={good} />
-      <p>Neutral: {neutral}</p>
-      <StatisticsLine text="neutral" value={neutral} />
-      <p>Bad: {bad}</p>
-      <StatisticsLine text="bad" value={bad} />
-      <p>all: {all}</p>
-      <StatisticsLine text="all" value={all} />
-      <p>average: {average({good, bad, all})}</p>
-      <StatisticsLine text="average" value={average({good, bad, all})} />
-      <p>positive: {positivePercentage(good, all)} %</p>
-      <StatisticsLine text="positive" value={positivePercentage(good, all)} />
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={all} />
+          <StatisticsLine text="average" value={average(good, bad, all)} />
+          <StatisticsLine text="positive" value={positivePercentage(good, all).toFixed(1)} percentage={true} />
+        </tbody>
+      </table>
     </div>
     )
   }
@@ -51,9 +50,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const all = good + neutral + bad
-
-
-  
 
   return (
     <div>

b4c76d57f4e22e987a979e422e063b58925357e2
technolifts <ryan.acarroll@yahoo.com>
2025-04-09 21:19:34 -0700

    Complete 3.4

--- part3/index.js
+++ part3/index.js
@@ -53,6 +53,13 @@ app.get('/info', (request, response) => {
     }
   })
 
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id == id)
+
+    response.status(204).end()
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
